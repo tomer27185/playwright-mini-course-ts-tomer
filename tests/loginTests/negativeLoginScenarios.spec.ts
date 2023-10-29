b46e8c8fc75b2f7f88c294f90e3ef5ba8b0f9b31
tomer.ashkenazi <tomer.ashkenazi@appsflyer.com>
2023-10-29 11:31:49 +0200

    refactoring positive test

--- tests/loginTests/negativeLoginScenarios.spec.ts
+++ tests/loginTests/negativeLoginScenarios.spec.ts
@@ -10,19 +10,19 @@ test.describe('Negative Login Scenarios', () => {
     loginPage = new LoginPage(page);
   });
 
-  test('Login with locked_out_user', async ({ page }) => {
+  test('Login with locked_out_user', async () => {
     await loginPage.loginToApplication(process.env.LOCKED_OUT_USER, process.env.CORRECT_PASSWORD);
     await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_LOCKED_USER);
     await loginPage.validatePageUrl(ApplicationURL.BASE_URL);
   });
 
-  test('Login with incorrect username', async ({ page }) => {
+  test('Login with incorrect username', async () => {
     await loginPage.loginToApplication('incorrect_user', process.env.CORRECT_PASSWORD);
     await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_INCORRECT_CREDENTIALS);
     await loginPage.validatePageUrl(ApplicationURL.BASE_URL);
   });
 
-  test('Login with incorrect password', async ({ page }) => {
+  test('Login with incorrect password', async () => {
     await loginPage.loginToApplication(process.env.STANDART_USER, '12345');
     await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_INCORRECT_CREDENTIALS);
     await loginPage.validatePageUrl(ApplicationURL.BASE_URL);
--- tests/loginTests/positiveLoginScenarios.spec.ts
+++ tests/loginTests/positiveLoginScenarios.spec.ts
@@ -1,25 +1,36 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../../pages/LoginPage';
 import ApplicationURL from '../../helpers/ApplicationURL';
+import ProductPage from '../../pages/ProductsPage';
+import PageTitles from '../../helpers/PageTitles';
 
-test.describe('Negative Login Scenarios', () => {
+test.describe('Positive Login Scenarios', () => {
   let loginPage: LoginPage;
+  let productPage: ProductPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
+    productPage = new ProductPage(page);
   });
 
-  test('Login with standard_user', async ({ page }) => {
-    await loginPage.loginToApplication(process.env.STANDARD_USER,process.env.CORRECT_PASSWORD);
+  test.afterEach(async () => {
+    await productPage.validateTitle(PageTitles.INVENTORY_PAGE);
+  });
+
+  test('Login with standard_user', async () => {
+    await loginPage.loginToApplication(
+      process.env.STANDARD_USER,
+      process.env.CORRECT_PASSWORD
+    );
     await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
   });
 
-  test('Login with problem_user', async ({ page }) => {
+  test('Login with problem_user', async () => {
     await loginPage.loginToApplication(process.env.PROBLEM_USER);
     await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
   });
 
-  test('Login with performance_glitch_user', async ({ page }) => {
+  test('Login with performance_glitch_user', async () => {
     await loginPage.loginToApplication(process.env.PERFORMANCE_GLITCH_USER);
     await loginPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
   });
